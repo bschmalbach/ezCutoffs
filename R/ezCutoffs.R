a1e96e5a208a6496b2b81d37cf977a62fe71ec89
Bjarne Schmalbach <55752891+bschmalbach@users.noreply.github.com>
2019-11-25 20:35:28 +0100

    correct n miss in simdata with complete miss cases

--- R/ezCutoffs.R
+++ R/ezCutoffs.R
@@ -166,6 +166,7 @@ ezCutoffs <- function(model = NULL,
   groups_var <- lavaan::lavInspect(fit, what = "group")
   n_groups <- lavaan::lavInspect(fit, what = "ngroups")
   group_labels <- lavaan::lavInspect(fit, what = "group.label")
+  dots <- list(...)
   # stop if grouping variable wiht only one level has been selected
   if ((length(groups_var) > 0) & n_groups < 2) {
     stop("Less than 2 levels in the defined grouping variable.")
@@ -219,6 +220,24 @@ ezCutoffs <- function(model = NULL,
     var_table <- lavaan::varTable(fit)
 
     missings <- apply(data, 2, function(x) which(is.na(x)))
+    n_var <- nrow(var_table)
+    
+    if (is.null(dots$missing)==T) {
+      dots$missing <- "listwise"
+    }
+    if (dots$missing == "fiml") { #full misses
+      misses <- as.integer(names(which(table(unlist(missings)) == n_var)))
+      } else { # listwise, any misses
+      misses <- as.integer(names(table(unlist(missings))))
+    }
+    
+    for (i in 1:n_var) {
+          pos <- missings[[i]] %in% misses
+          missings[[i]] <- missings[[i]][!pos]
+    }
+                      
+                  
+                      
     for (i in 1:n_rep) {
       for (v in 1:nrow(var_table)) {
         data_s_list[[i]][missings[[v]], v] <- NA # use same missing template as given data
