ba8b56bf2031c0bc9e41b0f2588e70e9bb8ee67e
Martin Schultze <schultze@psych.uni-frankfurt.de>
2019-12-03 14:10:44 +0100

    Replace plotting approach with general purpose ggplot2 usage.

--- R/plot.ezCutoffs.R
+++ R/plot.ezCutoffs.R
@@ -3,19 +3,39 @@ if (getRversion() >= "2.15.1") {
 }
 
 #' @export
-plot.ezCutoffs <- function(x, ...) {
-  for (i in 1:nrow(x[["summary"]])) {
-    hist_plots <- ggplot2::ggplot(x[["fitDistributions"]], ggplot2::aes_string(x = rownames(x[["summary"]])[i])) +
-      ggplot2::geom_histogram(ggplot2::aes(y = stat(count)), color = "black", fill = "white", bins = 30)
-    
-    bar_height <- max(ggplot2::ggplot_build(hist_plots)$data[[1]]$count)
-    
-    hist_plots <- hist_plots + ggplot2::geom_vline(xintercept = x[["summary"]][i, 1], color = "blue", alpha = 0.8) +
-      ggplot2::annotate("text", x = x[["summary"]][i, 1], y = bar_height, angle = 0, label = "Empirical") +
-      ggplot2::geom_vline(xintercept = x[["summary"]][i, 5], color = "red", alpha = 0.8) +
-      ggplot2::annotate("text", x = x[["summary"]][i, 5], y = bar_height, angle = 0, label = "Cutoff") +
-      ggplot2::labs(title = "Simulated Fit Distribution", y = "count") +
-      ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
-    (print(hist_plots))
+plot.ezCutoffs <- function(x, fit_indices = NULL, type = 'histogram', ...) {
+
+  summ <- x[['summary']]
+  if (is.null(fit_indices)) fit_indices <- rownames(summ)
+  
+  summ$fit_index <- rownames(summ)
+  names(summ)[grepl('Cutoff', names(summ))] <- 'Cutoff'
+  summ <- subset(summ, subset = summ$fit_index %in% fit_indices,
+    select = c('Empirical fit', 'Cutoff', 'fit_index'))
+  summ <- stats::reshape(summ, timevar = 'Reference', 
+    varying = list(c('Empirical fit', 'Cutoff')), v.names = 'value',
+    times = c('Empirical', 'Cutoff'), idvar = 'fit_index',
+    direction = 'long')
+  
+  disti <- x[['fitDistributions']]
+  disti <- subset(disti, select = fit_indices)
+  disti <- stats::reshape(disti, timevar = 'fit_index', 
+    varying = list(names(disti)), v.names = 'Fit',
+    direction = 'long',
+    times = names(disti))
+  
+  plottable <- ggplot2::ggplot(disti, ggplot2::aes(x = Fit, group = fit_index)) +
+    ggplot2::theme(title = ggplot2::element_text('Simulated Fit Distribution')) +
+    ggplot2::facet_wrap(~ fit_index, scales = 'free')
+
+  if (type == 'density') {
+    plottable <- plottable + ggplot2::geom_density()
+  } else {
+    plottable <- plottable + ggplot2::geom_histogram(ggplot2::aes(y = ..density..))
   }
+  
+  plottable <- plottable + 
+    ggplot2::geom_vline(data = summ, ggplot2::aes(xintercept = value, lty = Reference))
+  
+  return(plottable)
 }
